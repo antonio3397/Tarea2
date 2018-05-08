2dc471759c96a742a130de02bbf63bf10e09206c
migue <migue@192.168.1.103>
2018-05-09 00:55:14 +0200

    Perfil implementado a plantilla

--- src/java/controladores/Control_Perfil.java
+++ src/java/controladores/Control_Perfil.java
@@ -28,8 +28,8 @@ public class Control_Perfil {
     public void init(){
         
         usuarios = new ArrayList<>();
-        usuarios.add(new Usuario_apoyo(121, "123456", "78556410V", new Perfil_apoyo(Perfil_apoyo.Rol.ADMIN), "paco_mg99@hotmail.com", "Francisco", "Marin Garzon", "masc", "02/03/1997", 29610, "C/Luisa Ordoñez n15 1ºB", "Malaga", "02/03/2015", 921121314, 654121314, "tarjeta_credito", 50));
-        usuarios.add(new Usuario_apoyo(122, "234567", "71156411N", new Perfil_apoyo(Perfil_apoyo.Rol.EDUCANDO), "paula_vp@hotmail.com", "Paula", "Vergara Perez", "fem", "06/11/1997", 29615, "C/Santa Rosa n17 5ºC", "Malaga", "11/10/2015", 921675432, 654960584, "tarjeta_credito", 75));
+        usuarios.add(new Usuario_apoyo(121, "123456", "78556410V", new Perfil_apoyo(Perfil_apoyo.Rol.ADMIN), "paco_mg99@hotmail.com", "Francisco", "Marin Garzón", "Hombre", "02/03/1997", 29610, "C/Luisa Ordoñez n15 1ºB", "Málaga", "02/03/2015", 921121314, 654121314, "Tarjeta_Crédito", 50));
+        usuarios.add(new Usuario_apoyo(122, "234567", "71156411N", new Perfil_apoyo(Perfil_apoyo.Rol.EDUCANDO), "paula_vp@hotmail.com", "Paula", "Vergara Perez", "Mujer", "06/11/1997", 29615, "C/Santa Rosa n17 5ºC", "Málaga", "11/10/2015", 921675432, 654960584, "Tarjeta_Crédito", 75));
 
     }
 
