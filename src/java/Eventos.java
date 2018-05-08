8c48ced74817fb1c1b46c5a79a56aa25a938a9ae
anton <anton@DESKTOP-RS3GRJG>
2018-05-08 16:50:35 +0200

    Aun queda JSF

--- src/java/Eventos.java
+++ src/java/Eventos.java
@@ -4,16 +4,13 @@
  * and open the template in the editor.
  */
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import org.primefaces.event.TabChangeEvent;
 import org.primefaces.event.TabCloseEvent;
 
@@ -21,19 +18,18 @@ import org.primefaces.event.TabCloseEvent;
  *
  * @author anton
  */
-@Named(value = "eventos")
-@ManagedBean
-@SessionScoped
+@ManagedBean(name="Eventos")
+@ViewScoped
 public class Eventos{
 
     private List<Evento> eventosj;
     
     @PostConstruct
     public void init() {
-        eventosj = new ArrayList<Evento>();
-        getEventosj().add(new Evento(1, "Viaje al monte", "24/03/2018", "Córdoba", "Viaje a córdoba a una de las sierras mas bonitas", 20));
-        getEventosj().add(new Evento(2, "Viaje al monte 2", "24/06/2018", "Córdoba", "Viaje a córdoba a una de las sierras mas bonitas", 20));
-        getEventosj().add(new Evento(3, "Salvemos a las ardillas", "27/09/2019", "EEUU", "Viaje a EEUU para salvar a las ardillas", 1200));
+        eventosj = new ArrayList<>();
+        eventosj.add(new Evento(1, "Viaje al monte", "24/03/2018", "Córdoba", "Viaje a córdoba a una de las sierras mas bonitas", 20));
+        eventosj.add(new Evento(2, "Viaje al monte 2", "24/06/2018", "Córdoba", "Viaje a córdoba a una de las sierras mas bonitas", 20));
+        eventosj.add(new Evento(3, "Salvemos a las ardillas", "27/09/2019", "EEUU", "Viaje a EEUU para salvar a las ardillas", 1200));
     }
 
     /**
@@ -49,15 +45,4 @@ public class Eventos{
     public void setEventosj(List<Evento> eventosj) {
         this.eventosj = eventosj;
     }
-    
-    public void onTabChange(TabChangeEvent event) {
-        FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-         
-    public void onTabClose(TabCloseEvent event) {
-        FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-    
 }
