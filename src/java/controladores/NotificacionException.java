2d7fd2b40c837476f0c5263992b2b770632b6b0f
Apocalypsis <Apocalypsis@DESKTOP-BOD19UN.Home>
2018-05-12 19:07:39 +0200

    Cuerpo de notificaciones completo
    Notificación-> Constructor añadido y variable fecha añadida con sus get&set
    NotificaciónID-> Constructor añadido
    Control_Notificaciones-> Clase completada
    NotificacionException-> Clase completada
    Lista_Notificaciones-> Diseño funcional completado
    Plantilla-> Se añadio el direccionamiento a Lista_Notificaciones

---
+++ src/java/controladores/NotificacionException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+/**
+ *
+ * @author Apocalypsis
+ */
+public class NotificacionException extends Exception{
+    public NotificacionException(String msg){
+        super(msg);
+    }
+}
