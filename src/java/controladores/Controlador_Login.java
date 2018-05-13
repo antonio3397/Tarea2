fe3f79a41f46bc81df8539ae5cc4638516ad16fd
DavidDR <DavidDR@LAPTOP-D8V375SC>
2018-05-13 04:46:53 +0200

    Modificar usuario
    Funciona a medias -> Tengo que hacer que se cambie también en Controlador_Login

--- src/java/controladores/Controlador_Login.java
+++ src/java/controladores/Controlador_Login.java
@@ -35,7 +35,7 @@ public class Controlador_Login implements Serializable {
     private List<Usuario> users;
     private List<Evento> events;
     private Usuario otro;
-    
+
     /*private String nombremod;
     private String apellidosmod;
     private String nifmod;
@@ -141,171 +141,16 @@ public class Controlador_Login implements Serializable {
     }
 
     public String verUsuario(Long id) {
-        
-        for(Usuario a : users){
-            if(id.equals(a.getId())){
-                otro=a;
-            }
+
+        Iterator<Usuario> iter = users.iterator();
+        Usuario u = iter.next();
+        while(iter.hasNext()&& id != u.getId()) {
+            u = iter.next();
         }
-        
-        return "OtroPerfil.xhtml";
-    }
-    
-    public String modificarboton(Long id){
-        
-        for(Usuario a : users){
-            if(id.equals(a.getId())){
-                otro=a;
-            }
+        if (id == u.getId()) {
+            otro = u;
         }
-        
-        //usermod=new Usuario(id, u.getContrasenia(), u.getNIF(), u.getEmail(), u.getNombre(), u.getApellidos(), u.getSexo(), u.getFecha_nacimiento(), u.getCodigo_postal(),u.getDireccion(), u.getProvincia(), u.getLocalidad(), u.getFecha_ingreso(), u.getCuota_total(), u.getTelefono(), u.getMovil(), u.getMetodo_pago(), u.getPerfiles(), u.getSeccion());
-        
-        /*usermod.setId(id);
-        usermod.setNombre(u.getNombre());
-        usermod.setApellidos(u.getApellidos());
-        usermod.setContrasenia(u.getContrasenia());
-        usermod.setNIF(u.getNIF());
-        usermod.setEmail(u.getEmail());
-        usermod.setSexo(u.getSexo());
-        usermod.setFecha_nacimiento(u.getFecha_nacimiento());
-        usermod.setCodigo_postal(u.getCodigo_postal());
-        usermod.setDireccion(u.getDireccion());
-        usermod.setProvincia(u.getProvincia());
-        usermod.setLocalidad(u.getLocalidad());
-        usermod.setFecha_ingreso(u.getFecha_ingreso());
-        usermod.setFecha_baja(u.getFecha_baja());
-        usermod.setCuota_total(u.getCuota_total());
-        usermod.setTelefono(u.getTelefono());
-        usermod.setMovil(u.getMovil());
-        usermod.setMetodo_pago(u.getMetodo_pago());
-        usermod.setDocumentos(u.getDocumentos());
-        usermod.setComentarios(u.getComentarios());
-        usermod.setPerfiles(u.getPerfiles());
-        usermod.setPago(u.getPago());
-        usermod.setSeccion(u.getSeccion());
-        usermod.setEventos(u.getEventos());
-        usermod.setNotificaciones(u.getNotificaciones());
-        usermod.setInscripciones(u.getInscripciones());
-        usermod.setResponsable(u.getResponsable());*/
-        /*nombremod=u.getNombre();
-        apellidosmod=u.getApellidos();
-        nifmod=u.getNIF();
-        sexomod=u.getSexo();
-        emailmod=u.getEmail();
-        nacimientomod=u.getFecha_nacimiento();
-        codigopostalmod=u.getCodigo_postal();
-        direcmod=u.getDireccion();
-        provmod=u.getProvincia();
-        localmod=u.getLocalidad();
-        ingresomod=u.getFecha_ingreso();
-        cuotamod=u.getCuota_total();
-        telefonomod=u.getTelefono();
-        movilmod=u.getMovil();
-        metodopagomod=u.getMetodo_pago();*/
-        
-        return "ModPerf.xhtml";
-    }
-    
-    public String aceptarmod(){
-        
-        Seccion sec;
-        
-        switch (seccionmod) {
-            case "Castores":
-                sec= new Seccion(1L, Seccion.Secciones.Castores);
-                break;
-            case "Lobatos":
-                sec= new Seccion(2L, Seccion.Secciones.Lobatos);
-                break;
-            case "Scouts":
-                sec= new Seccion(4L, Seccion.Secciones.Tropa_Scout);
-                break;
-            case "Escultas":
-                sec= new Seccion(5L, Seccion.Secciones.Escultas_Pioneros);
-                break;
-            case "Rovers":
-                sec= new Seccion(3L, Seccion.Secciones.Rovers_Compañeros);
-                break;
-            default:
-                sec= otro.getSeccion();
-                break;
-        }
-        
-        otro.setSeccion(sec);
-        
-        int i=0;
-        boolean enc=false;
-        while(i<users.size() && !enc){
-            if(Objects.equals(users.get(i).getId(), otro.getId())){
-                enc=true;
-            }
-            i++;
-        }
-        /*users.get(i-1).setNombre(nombremod);
-        users.get(i-1).setApellidos(apellidosmod);
-        users.get(i-1).setNIF(nifmod);
-        users.get(i-1).setSexo(sexomod);
-        users.get(i-1).setEmail(emailmod);
-        users.get(i-1).setFecha_nacimiento(nacimientomod);
-        users.get(i-1).setCodigo_postal(codigopostalmod);
-        users.get(i-1).setDireccion(direcmod);
-        users.get(i-1).setProvincia(provmod);
-        users.get(i-1).setLocalidad(localmod);
-        users.get(i-1).setFecha_ingreso(ingresomod);
-        users.get(i-1).setCuota_total(cuotamod);
-        users.get(i-1).setTelefono(telefonomod);
-        users.get(i-1).setMovil(movilmod);
-        users.get(i-1).setMetodo_pago(metodopagomod);
-        users.get(i-1).setSeccion(sec);*/
-        users.get(i-1).setNombre(otro.getNombre());
-        users.get(i-1).setApellidos(otro.getApellidos());
-        users.get(i-1).setNIF(otro.getNIF());
-        users.get(i-1).setSexo(otro.getSexo());
-        users.get(i-1).setEmail(otro.getEmail());
-        users.get(i-1).setFecha_nacimiento(otro.getFecha_nacimiento());
-        users.get(i-1).setCodigo_postal(otro.getCodigo_postal());
-        users.get(i-1).setDireccion(otro.getDireccion());
-        users.get(i-1).setProvincia(otro.getProvincia());
-        users.get(i-1).setLocalidad(otro.getLocalidad());
-        users.get(i-1).setFecha_ingreso(otro.getFecha_ingreso());
-        users.get(i-1).setCuota_total(otro.getCuota_total());
-        users.get(i-1).setTelefono(otro.getTelefono());
-        users.get(i-1).setMovil(otro.getMovil());
-        users.get(i-1).setMetodo_pago(otro.getMetodo_pago());
-        users.get(i-1).setSeccion(otro.getSeccion());
-        
-        return "OtroPerfil.xhtml";
-    }
-    
-    public String cancelarmod(){
-        
-        int i=0;
-        boolean enc=false;
-        while(i<users.size() && !enc){
-            if(Objects.equals(users.get(i).getId(), otro.getId())){
-                enc=true;
-            }
-            i++;
-        }
-        
-        otro.setNombre(users.get(i-1).getNombre());
-        otro.setApellidos(users.get(i-1).getApellidos());
-        otro.setNIF(users.get(i-1).getNIF());
-        otro.setSexo(users.get(i-1).getSexo());
-        otro.setEmail(users.get(i-1).getEmail());
-        otro.setFecha_nacimiento(users.get(i-1).getFecha_nacimiento());
-        otro.setCodigo_postal(users.get(i-1).getCodigo_postal());
-        otro.setDireccion(users.get(i-1).getDireccion());
-        otro.setProvincia(users.get(i-1).getProvincia());
-        otro.setLocalidad(users.get(i-1).getLocalidad());
-        otro.setFecha_ingreso(users.get(i-1).getFecha_ingreso());
-        otro.setCuota_total(users.get(i-1).getCuota_total());
-        otro.setTelefono(users.get(i-1).getTelefono());
-        otro.setMovil(users.get(i-1).getMovil());
-        otro.setMetodo_pago(users.get(i-1).getMetodo_pago());
-        otro.setSeccion(users.get(i-1).getSeccion());
-        
+
         return "OtroPerfil.xhtml";
     }
 
--- src/java/controladores/MiSesion.java
+++ src/java/controladores/MiSesion.java
@@ -13,7 +13,9 @@ import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
 /*
 Tipos de id que tiene cada sección:
@@ -31,17 +33,22 @@ ESCULTAS PIONEROS -> 5L
 @Named(value = "miSesion")
 @SessionScoped
 public class MiSesion implements Serializable {
-
+    
     private Usuario user;
     private List<Usuario> users;
     private List<Usuario> users2;
+    private Usuario otro;
+    private String seccionmod;
+    
+    @Inject
+    Controlador_Login ctr;
 
     /**
      * Creates a new instance of MiSesion
      */
     public MiSesion() {
     }
-
+    
     public String logout() {
         // Destruye la sesión (y con ello, el ámbito de este bean)
         FacesContext ctx = FacesContext.getCurrentInstance();
@@ -49,11 +56,11 @@ public class MiSesion implements Serializable {
         user = null;
         return "login.xhtml";
     }
-
+    
     public Usuario buscarUsuario(Long id) throws UsuarioException {
-
+        
         Usuario aux = null;
-
+        
         Iterator<Usuario> iter = users.iterator();
         while (iter.hasNext() && aux == null) {
             Usuario it = iter.next();
@@ -61,21 +68,90 @@ public class MiSesion implements Serializable {
                 aux = it;
             }
         }
-
+        
         if (aux == null) {
             throw new UsuarioException("Usuarios no existente");
         }
-
+        
         return aux;
     }
-
+    
+    public String modificarboton(Long id) {
+        
+        Usuario u = null;
+        Iterator<Usuario> iter = users.iterator();
+        u = iter.next();
+        while (iter.hasNext() && u.getId() == id) {
+            u = iter.next();
+        }
+        
+        setOtro(new Usuario(u.getId(), u.getContrasenia(), u.getNIF(), u.getEmail(), u.getNombre(), u.getApellidos(), u.getSexo(), u.getFecha_nacimiento(), u.getCodigo_postal(), u.getDireccion(), u.getProvincia(), u.getLocalidad(), u.getFecha_ingreso(), u.getCuota_total(), u.getTelefono(), u.getMovil(), u.getMetodo_pago(), u.getPerfiles(), u.getSeccion()));
+        
+        return "ModPerf.xhtml";
+    }
+    
+    public String aceptarmod() {
+        
+        Seccion sec;
+        
+        switch (getSeccionmod()) {
+            case "Castores":
+                sec = new Seccion(1L, Seccion.Secciones.Castores);
+                break;
+            case "Lobatos":
+                sec = new Seccion(2L, Seccion.Secciones.Lobatos);
+                break;
+            case "Scouts":
+                sec = new Seccion(4L, Seccion.Secciones.Tropa_Scout);
+                break;
+            case "Escultas":
+                sec = new Seccion(5L, Seccion.Secciones.Escultas_Pioneros);
+                break;
+            case "Rovers":
+                sec = new Seccion(3L, Seccion.Secciones.Rovers_Compañeros);
+                break;
+            default:
+                sec = otro.getSeccion();
+                break;
+        }
+        
+        otro.setSeccion(sec);
+        
+        int i = 0;
+        while (i < users.size() && otro.getId() != users.get(i).getId()) {
+            i++;
+        }
+        
+        Usuario u = users.get(i);
+        u.setNombre(otro.getNombre());
+        u.setApellidos(otro.getApellidos());
+        u.setNIF(otro.getNIF());
+        u.setSexo(otro.getSexo());
+        u.setEmail(otro.getEmail());
+        u.setFecha_nacimiento(otro.getFecha_nacimiento());
+        u.setCodigo_postal(otro.getCodigo_postal());
+        u.setDireccion(otro.getDireccion());
+        u.setProvincia(u.getProvincia());
+        u.setLocalidad(otro.getLocalidad());
+        u.setFecha_ingreso(otro.getFecha_ingreso());
+        u.setCuota_total(otro.getCuota_total());
+        u.setTelefono(otro.getTelefono());
+        u.setMovil(otro.getMovil());
+        u.setMetodo_pago(otro.getMetodo_pago());
+        u.setSeccion(otro.getSeccion());
+        
+        ctr.setUsers(users);
+        
+        return "Lista_Usuarios.xhtml";
+    }
+    
     public String borrarUsuario(Long id) throws UsuarioException {
-
+        
         Usuario b = buscarUsuario(id);
-
+        
         users.remove(b);
         users2.remove(b);
-
+        
         return "Lista_Usuarios.xhtml";
     }
 
@@ -92,48 +168,56 @@ public class MiSesion implements Serializable {
     public void setUsers(List<Usuario> users) {
         this.users = users;
     }
-
+    
     public Usuario getUser() {
         return user;
     }
-
+    
     public void setUser(Usuario user) {
         this.user = user;
     }
-
+    
     public boolean isCoordGen() {
         return this.user.getPerfiles().getRol().equals(Perfil.Rol.COORDGEN);
     }
-
+    
     public boolean isCordSec() {
         return this.user.getPerfiles().getRol().equals(Perfil.Rol.COORDSEC);
     }
-
+    
     public boolean isScouter() {
         return this.user.getPerfiles().getRol().equals(Perfil.Rol.SCOUTER);
     }
-
+    
     public boolean isEducando() {
         return this.user.getPerfiles().getRol().equals(Perfil.Rol.EDUCANDO);
     }
     
     public String getSeccion() {
         String salida = "";
-        if (user.getSeccion().getNombre().equals(Seccion.Secciones.Castores)) {
-            salida = "Castores";
-        } else if (user.getSeccion().getNombre().equals(Seccion.Secciones.Escultas_Pioneros)){
-            salida = "Escultas";
-        } else if (user.getSeccion().getNombre().equals(Seccion.Secciones.Lobatos)) {
-            salida = "Lobatos";
-        } else if (user.getSeccion().getNombre().equals(Seccion.Secciones.Rovers_Compañeros)) {
-            salida = "Rovers";
-        } else if (user.getSeccion().getNombre().equals(Seccion.Secciones.Tropa_Scout)) {
-            salida = "Scouts";
+        switch (user.getSeccion().getNombre()) {
+            case Castores:
+                salida = "Castores";
+                break;
+            case Escultas_Pioneros:
+                salida = "Escultas";
+                break;
+            case Lobatos:
+                salida = "Lobatos";
+                break;
+            case Rovers_Compañeros:
+                salida = "Rovers";
+                break;
+            case Tropa_Scout:
+                salida = "Scouts";
+                break;
+            default:
+                break;
         }
         
-       return salida;
+        return salida;
     }
-    
+
     /**
      * @return the users2
      */
@@ -148,4 +232,32 @@ public class MiSesion implements Serializable {
         this.users2 = users2;
     }
 
+    /**
+     * @return the otro
+     */
+    public Usuario getOtro() {
+        return otro;
+    }
+
+    /**
+     * @param otro the otro to set
+     */
+    public void setOtro(Usuario otro) {
+        this.otro = otro;
+    }
+
+    /**
+     * @return the seccionmod
+     */
+    public String getSeccionmod() {
+        return seccionmod;
+    }
+
+    /**
+     * @param seccionmod the seccionmod to set
+     */
+    public void setSeccionmod(String seccionmod) {
+        this.seccionmod = seccionmod;
+    }
+    
 }
